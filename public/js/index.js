1cf08c31cbc958da5524b313e8c2a63404df50b8
ninadlohar <ninad.lohar94@gmail.com>
2019-02-04 13:25:37 +0530

    added moment.js and render timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -11,9 +11,9 @@ socket.on('disconnect', function() {
 })
 
 socket.on('newMessage', function(message) {
-  console.log('newMessage', message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`)
+  var formattedTime = moment(message.createdAt).format('h:mm a')
+  li.text(`${message.from}: ${formattedTime} ${message.text}`)
   jQuery('#messages').append(li)
 })
 
@@ -29,9 +29,10 @@ jQuery('#message-form').on('submit', function(e) {
 })
 
 socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a')
   var li = jQuery(`<li></li>`)
   var a = jQuery('<a target="_blank">My current location</a>')
-  li.text(`${message.from}:`);
+  li.text(`${message.from}: ${formattedTime}`);
   a.attr('href', message.url)
   li.append(a)
   jQuery('#messages').append(li)
